64c3f674a2f34384dd6223c07988d3e74a416908
Luis Enrique Moreno Arrescurenaga <luis.moreno@vfcons.com>
2018-06-08 22:00:25 -0500

    Impuesto a la renta
    Impuesto a la renta

---
+++ loan-MORENO/src/main/java/edu/progra2/loanMORENO/LoanMorenoApplication.java
@@ -0,0 +1,12 @@
+package edu.progra2.loanMORENO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanMorenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanMorenoApplication.class, args);
+	}
+}
